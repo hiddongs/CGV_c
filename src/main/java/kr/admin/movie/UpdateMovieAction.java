680701b0e0fb55f358e3198e202a5c661ff04291
user <user@DESKTOP-S6UUK9H>
2025-05-09 15:43:24 +0900

    미디어 delete 완

--- src/main/java/kr/admin/movie/UpdateMovieAction.java
+++ src/main/java/kr/admin/movie/UpdateMovieAction.java
@@ -24,30 +24,30 @@ public class UpdateMovieAction implements Action{
 		String oldPosterUrl = req.getParameter("oldPosterUrl");
 		String posterUrl = req.getParameter("poster_url");
 		
-		MovieVO movieVO = new MovieVO();
-		movieVO.setMovie_id(Integer.parseInt(req.getParameter("movie_id")));
-		movieVO.setMv_title(req.getParameter("title"));
+		MovieVO vo = new MovieVO();
+		vo.setMovie_id(Integer.parseInt(req.getParameter("movie_id")));
+		vo.setMv_title(req.getParameter("title"));
 		
-		if(req.getParameter("deletePosterUrl").isEmpty()){
-			movieVO.setPoster_url(oldPosterUrl);
+		if(posterUrl.isEmpty()){
+			vo.setPoster_url(oldPosterUrl);
 		}else {
 			FileUtil.removeFile(req, oldPosterUrl);
 			FileUtil.uploadFile(req, posterUrl);
-			movieVO.setPoster_url(posterUrl);
+			vo.setPoster_url(posterUrl);
 		}
-		movieVO.setDirector(req.getParameter("director"));
-		movieVO.setActor(req.getParameter("actor"));
+		vo.setDirector(req.getParameter("director"));
+		vo.setActor(req.getParameter("actor"));
 		
 		String genres = String.join(",", req.getParameter("genre"));
-		movieVO.setGenre(genres);
-		movieVO.setAge_limit(req.getParameter("age_limit"));
-		movieVO.setRelease_date(DBUtil.toSqlDate(req.getParameter("release_date")));
-		movieVO.setRuntime(Integer.parseInt(req.getParameter("runtime")));
-		movieVO.setDescription(req.getParameter("description"));
+		vo.setGenre(genres);
+		vo.setAge_limit(req.getParameter("age_limit"));
+		vo.setRelease_date(DBUtil.toSqlDate(req.getParameter("release_date")));
+		vo.setRuntime(Integer.parseInt(req.getParameter("runtime")));
+		vo.setDescription(req.getParameter("description"));
 		
 		MovieDAO dao = MovieDAO.getInstance();
 		
-		int result = dao.updateMovie(movieVO);
+		int result = dao.updateMovie(vo);
 		
 		if(result == 1) {
 			req.setAttribute("result_title", "영화 수정 성공");
