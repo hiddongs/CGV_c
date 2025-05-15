9895791f187942d0d3bae0acdf9211dd8b498e4a
hiddongs <yhd1227@naver.com>
2025-05-15 11:08:42 +0900

    충돌 확인

--- src/main/java/kr/ask/dao/AskDAO.java
+++ src/main/java/kr/ask/dao/AskDAO.java
@@ -1,5 +1,17 @@
 package kr.ask.dao;
 
+import java.util.Map;
+
 public class AskDAO {
+	public Map<String,String> map;
+
+	public Map<String, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+	
 	
 }
